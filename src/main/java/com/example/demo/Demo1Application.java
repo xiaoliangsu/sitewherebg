a7fa66dc66945140d1beac0620ae753b68ca1e39
zhangyunlong <496933982@qq.com>
2018-05-22 21:43:37 +0800

    change to device service

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,18 +2,13 @@ package com.example.demo;
 
 import config.GlobalCorsConfig;
 import controller.AddDeviceController;
-import controller.DeviceDetailController;
-import controller.MeasureController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import controller.UserController;
 
 @RestController
 @SpringBootApplication
@@ -28,7 +23,7 @@ public class Demo1Application implements EmbeddedServletContainerCustomizer{
     }
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserController.class, args);
+		SpringApplication.run(AddDeviceController.class, args);
 //		SpringApplication.run(AddDeviceController.class, args);
 //
 //		SpringApplication.run(MeasureController.class, args);
